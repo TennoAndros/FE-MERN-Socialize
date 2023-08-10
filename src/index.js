7d3c3c1758e0ca516da8b6141d4631dd0e4ba287
Andreas <andreasjr77@outlook.com>
2023-08-10 17:01:47 +0300

    Auth works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/ReduxStore";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
+import { useDispatch, useSelector } from "react-redux";
+import { logIn, signUp } from "../../actions/AuthAction";
 
 const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authReducer.loading);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [confirmPass, setConfirmPass] = useState(true);
+
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isSignUp) {
+      data.password === data.confirmPassword
+        ? dispatch(signUp(data))
+        : setConfirmPass(false);
+    } else {
+      dispatch(logIn(data));
+    }
+  };
+
+  const resetForm = () => {
+    setConfirmPass(true);
+    setData({
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
   return (
     <div className="Auth">
       <div className="Auth-Left">
@@ -12,111 +54,99 @@ const Auth = () => {
           <h5>Connect with people</h5>
         </div>
       </div>
-      <LogIn />
+      <div className="Auth-Right">
+        <form className="InfoForm AuthForm" onSubmit={handleSubmit}>
+          <h3>{isSignUp ? "Sign up" : "Log in"}</h3>
+          {isSignUp && (
+            <div>
+              <input
+                type="text"
+                className="InfoInput"
+                name="firstName"
+                placeholder="First Name"
+                aria-label="Enter first name"
+                onChange={handleChange}
+                value={data.firstName}
+              />
+              <input
+                type="text"
+                className="InfoInput"
+                name="lastName"
+                placeholder="Last Name"
+                aria-label="Enter last name"
+                onChange={handleChange}
+                value={data.lastName}
+              />
+            </div>
+          )}
+          <div>
+            <input
+              type="text"
+              className="InfoInput"
+              name="username"
+              placeholder="Username"
+              aria-label="Enter a username"
+              onChange={handleChange}
+              value={data.username}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              className="InfoInput"
+              name="password"
+              placeholder="Password"
+              aria-label="Enter a password"
+              onChange={handleChange}
+              value={data.password}
+            />
+            {isSignUp && (
+              <input
+                type="password"
+                className="InfoInput"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                aria-label="Confirm password"
+                onChange={handleChange}
+                value={data.confirmPassword}
+              />
+            )}
+          </div>
+          <span
+            style={{
+              display: confirmPass ? "none" : "block",
+              color: "red",
+              fontSize: "12px",
+              alignSelf: "flex-end",
+              marginRight: "5px",
+            }}
+          >
+            * Confirm Password doesn't match!
+          </span>
+          <div>
+            <span
+              style={{ fontSize: "14px", cursor: "pointer" }}
+              onClick={() => {
+                setIsSignUp((prev) => !prev);
+                resetForm();
+              }}
+            >
+              {isSignUp
+                ? "Already have an account? Login!"
+                : "Don't have an account? Sign Up."}
+            </span>
+          </div>
+          <button
+            className="Button InfoButton"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Loading..." : isSignUp ? "Sign up" : "Log in"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
-function LogIn() {
-  return (
-    <div className="Auth-Right">
-      <form className="InfoForm AuthForm">
-        <h3>Log In</h3>
-
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            className="InfoInput"
-            name="Username"
-          />
-        </div>
-
-        <div>
-          <input
-            type="Password"
-            className="InfoInput"
-            placeholder="Password"
-            name="Password"
-          />
-        </div>
-
-        <div>
-          <span style={{ fontSize: "12px" }}>
-            Don't have an account? Sign up!
-          </span>
-          <button className="Button InfoButton">Login</button>
-        </div>
-      </form>
-    </div>
-  );
-}
-function SignUp() {
-  return (
-    <div className="Auth-Right">
-      <form className="InfoForm AuthForm">
-        <h3>Sign up</h3>
-        <div>
-          <input
-            type="text"
-            className="InfoInput"
-            name="FirstName"
-            placeholder="First Name"
-            aria-label="Enter first name"
-          />
-          <input
-            type="text"
-            className="InfoInput"
-            name="LastName"
-            placeholder="Last Name"
-            aria-label="Enter last name"
-          />
-        </div>
-
-        <div>
-          <input
-            type="email"
-            className="InfoInput"
-            name="Email"
-            placeholder="E-mail"
-            aria-label="Enter an email"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            className="InfoInput"
-            name="UserName"
-            placeholder="Username"
-            aria-label="Enter a username"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            className="InfoInput"
-            name="Password"
-            placeholder="Password"
-            aria-label="Enter a password"
-          />
-          <input
-            type="text"
-            className="InfoInput"
-            name="ConfirmPassword"
-            placeholder="Confirm Password"
-            aria-label="Confirm password"
-          />
-        </div>
-        <div>
-          <span style={{ fontSize: "14px" }}>
-            Already have an account? Login!
-          </span>
-        </div>
-        <button className="Button InfoButton" typer="submit">
-          Sign up
-        </button>
-      </form>
-    </div>
-  );
-}
 
 export default Auth;
